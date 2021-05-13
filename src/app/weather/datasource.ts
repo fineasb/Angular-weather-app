618603c3e8342e539feb2ebc96d3cce4fdea7478
Fineas Berindei <fberindei@dexwell.com>
2021-05-13 03:44:51 -0700

    final app

--- src/app/weather/datasource.ts
+++
@@ -1,8 +0,0 @@
-export const columnData = [
-            { month: 'Jan', sales: 35 }, { month: 'Feb', sales:35 },
-            { month: 'Mar', sales: 35 }, { month: 'Apr', sales:35 },
-            { month: 'May', sales: 35 }, { month: 'Jun', sales: 32 },
-            { month: 'Jul', sales: 35 }, { month: 'Aug', sales: 55 },
-            { month: 'Sep', sales: 35 }, { month: 'Oct', sales: 30 },
-            { month: 'Nov', sales: 35 }, { month: 'Dec', sales: 32 }
-]
